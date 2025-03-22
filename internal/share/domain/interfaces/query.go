01493e551a21d73342b006ba8e5fa881010bf977
char5742 <fujino@stellarcode.jp>
2025-03-22 22:25:21 +0900

    refactor(interfaces): Eventインターフェースを独立したファイルに移動
    - workflow.goからEventインターフェースを抽出し、event.goに移動
    - 不要になったquery.goとworkflow.goを削除

--- internal/share/domain/interfaces/query.go
+++
@@ -1,6 +0,0 @@
-package shareinterfaces
-
-import sharetypes "github.com/char5742/ecsite-ddd-go/internal/share/domain/types"
-
-// ジェネリックなクエリハンドラー
-type QueryHandler[T, R any] = func(sharetypes.Query[T]) (R, error)
--- internal/share/domain/interfaces/workflow.go
+++
@@ -1,9 +0,0 @@
-package shareinterfaces
-
-import share_types "github.com/char5742/ecsite-ddd-go/internal/share/domain/types"
-
-type Workflow[T any] = func(share_types.Command[T]) ([]Event, error)
-
-type Event interface {
-	IsEvent()
-}
